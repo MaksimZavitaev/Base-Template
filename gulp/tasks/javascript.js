a2b455ba6c28ba2ba10a99f886b738b808c813f6
Maxim Zavitaev <vskweb@gmail.com>
2015-05-29 00:28:25 +0300

    Нотификация по сборке проекта

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -7,28 +7,44 @@ gulp.task('js:ie', function () {
     return gulp.src(config.js.ie)
         .pipe($.concat('ie.js'))
         .pipe($.uglify())
-        .pipe(gulp.dest(PUBLIC_FOLDER + '/js'));
+        .pipe(gulp.dest(PUBLIC_FOLDER + '/js'))
+        .pipe($.notify({
+            title: 'IE Libraries',
+            message: 'Успешно скомпилировано'
+        }));
 });
 
 gulp.task('js:vendor', function () {
     return gulp.src(config.js.vendor)
         .pipe($.concat('vendor.min.js'))
         .pipe($.uglify())
-        .pipe(gulp.dest(PUBLIC_FOLDER + '/js'));
+        .pipe(gulp.dest(PUBLIC_FOLDER + '/js'))
+        .pipe($.notify({
+            title: 'Vendor Libraries',
+            message: 'Успешно скомпилировано'
+        }));
 });
 
 gulp.task('js:app:build', function () {
     return gulp.src(config.js.app)
         .pipe($.concat('app.js'))
         .pipe(gulp.dest(PUBLIC_FOLDER + '/js'))
-        .pipe($.connect.reload());
+        .pipe($.connect.reload())
+        .pipe($.notify({
+            title: 'Application JavaScript',
+            message: 'Успешно скомпилировано'
+        }));
 });
 
 gulp.task('js:app:minify', ['js:app:build'], function () {
     return gulp.src(PUBLIC_FOLDER + '/js/app.js')
         .pipe($.uglify())
         .pipe($.rename('app.min.js'))
-        .pipe(gulp.dest(PUBLIC_FOLDER + '/js'));
+        .pipe(gulp.dest(PUBLIC_FOLDER + '/js'))
+        .pipe($.notify({
+            title: 'Application JavaScript',
+            message: 'Успешно минифицировано'
+        }));
 });
 
 gulp.task('js:app:watch', ['js:app:build'], function () {
