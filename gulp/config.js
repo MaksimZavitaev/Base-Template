b86e099ecd6a74651a3aac86dceaa93df1025b9e
Maxim Zavitaev <vskweb@gmail.com>
2015-05-28 19:41:56 +0300

    Сборка библиотек для совместимости с IE

--- gulp/config.js
+++ gulp/config.js
@@ -3,5 +3,13 @@ global.config = {
         folder: "./public",
         port: 8080,
         liveReload: true
+    },
+    js: {
+        ie: [
+            './bower_components/es5-shim/es5-shim.js',
+            './bower_components/html5shiv/dist/html5shiv.js',
+            './bower_components/html5shiv/dist/html5shiv-printshiv.js',
+            './bower_components/respond/dest/respond.src.js'
+        ]
     }
 };
---
+++ gulp/tasks/javascript.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')();
+
+gulp.task('js:ie', function () {
+    return gulp.src(config.js.ie)
+        .pipe($.concat('ie.js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest('./public/js'));
+});
